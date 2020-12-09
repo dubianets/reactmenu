a171c5b1ac7c87dfdd94b5ae5712a43d4a4b1204
dubianets <vadikdubianets@gmail.com>
2020-12-09 14:14:48 -0800

    add commit

--- src/App.js
+++ src/App.js
@@ -15,12 +15,10 @@ function App() {
         {make: 'Toyota', model: 'Camry', price:'1000'},
         {make: 'Mazda', model: 'CX', price:'3600'},
         {make: 'VAZ', model: 'Mh', price:'10'},
-        {make: 'VAZ', model: 'Mh', price:'10'},
     ];
 
     const city = [
         {title: "New York", sccore:99},
-        {title: "Seattle", sccore: 105},
         {title: "Seattle", sccore: 105},
         {title:"Maiami", sccore: 87},
     ]

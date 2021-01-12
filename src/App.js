fca4f20e47301ff46e2866f3dfa4936b4f5aa666
dubianets <vadikdubianets@gmail.com>
2021-01-11 17:50:43 -0800

    add commitadd commit

--- src/App.js
+++ src/App.js
@@ -45,6 +45,8 @@ function App() {
     const gifts = [
         {name: "Car", price: 10000},
         {name: "Carer", price: 100000},
+        {name: "Carer", price: 100000},
+        {name: "Carer", price: 100000},
         {name: "Camera", price: 2000},
         {name: "Candy", price: 100},
    ]

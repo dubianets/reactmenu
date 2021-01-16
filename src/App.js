5971f2b40ed6722ea1a0e8cc3b1b1a7ee1e6f1d5
dubianets <vadikdubianets@gmail.com>
2021-01-15 16:55:12 -0800

    add commitadd commit

--- src/App.js
+++ src/App.js
@@ -19,14 +19,12 @@ function App() {
 
     const city = [
        {title: "New York", sccore:99},
-       {title: "New York", sccore:99},
        {title: "Seattle", sccore: 105},
         {title:"Maiami", sccore: 87},
         {title:"Maiami", sccore: 87},
     ]
     const nyEmploey = [
         {title:"John", reiting: 68},
-        {title:"Smith", reiting: 80},
         {title:"Smith", reiting: 80},
         {title:"Yoker", reiting: 89},
       {title:"Yokejujr", reiting: 87},

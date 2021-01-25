c76ea54adf1f331e31164d79df3400f15bc2eb2e
dubianets <vadikdubianets@gmail.com>
2021-01-24 19:03:45 -0800

    add commitadd commit

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ function App() {
 
     const list = [
         {make: 'Toyota', model: 'Camry', price:'1000'},
-        {make: 'Toyota', model: 'Camry', price:'1000'},
-        {make: 'Toyota', model: 'Camry', price:'1000'},
-        {make: 'Toyota', model: 'Camry', price:'1000'},
         {make: 'Mazda', model: 'CX', price:'3600'},
         {make: 'VAZ', model: 'Mh', price:'10'},
     ];

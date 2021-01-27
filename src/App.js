f9f22fa8615bdd826ff4b1ce2a2d89c43eef406d
dubianets <vadikdubianets@gmail.com>
2021-01-26 18:28:29 -0800

    add commitadd commit

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
     const miEmploey =[
         {title:"Tomson", reiting: 67},
         {title:"West", reiting: 45},
+        {title:"West", reiting: 45},
         {title:"Narow", reiting: 56},
     ]
 

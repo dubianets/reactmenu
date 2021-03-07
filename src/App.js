63a05bbb7b2612f91ec3312157d27fe244eaeee8
dubianets <vadikdubianets@gmail.com>
2021-03-06 19:02:09 -0800

    addadd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
 
     const list = [
         {make: 'Toyota', model: 'Camry', price:'1000'},
+        {make: 'Toyota', model: 'Camry', price:'1000'},
         {make: 'Mazda', model: 'CX', price:'3600'},
+        {make: 'VAZ', model: 'Mh', price:'10'},
         {make: 'VAZ', model: 'Mh', price:'10'},
     ];
 

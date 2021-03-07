31dea532f220072b11fc68d525371ab076ba3e1a
dubianets <vadikdubianets@gmail.com>
2021-03-06 19:02:31 -0800

    add

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
         {make: 'Toyota', model: 'Camry', price:'1000'},
         {make: 'Toyota', model: 'Camry', price:'1000'},
         {make: 'Mazda', model: 'CX', price:'3600'},
-        {make: 'VAZ', model: 'Mh', price:'10'},
-        {make: 'VAZ', model: 'Mh', price:'10'},
     ];
 
     const city = [
